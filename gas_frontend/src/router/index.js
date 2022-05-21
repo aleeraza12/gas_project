3430dd5d1023b13eb3858cb13525c7a9101db6f9
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-05-21 16:04:53 +0500

    changeds

---
+++ gas_frontend/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/signup",
+      name: "Sign up",
+      meta: { layout: "simple" , title:"Sing Up" },
+      component: require("@/views/Pages/SignUp").default
+    },
+    {
+      path: "/",
+      name: "Home",
+      meta: { layout: "simple" , title:"Login" },
+      component: require("@/views/Pages/Login").default
+    },
+    {
+      path: "/login",
+      name: "Login",
+      meta: { layout: "simple" , title:"Login" },
+      component: require("@/views/Pages/Login").default
+    },
+   
+  ]
+});
+router.beforeEach(async(to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!localStorage.getItem("token")) {
+      next({
+        name: "Login"
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
