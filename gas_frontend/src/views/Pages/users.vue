74790c1c6019d162fa13109fd73abb203e7b8ee5
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-05-28 12:43:57 +0500

    chnagews

---
+++ gas_frontend/src/views/Pages/users.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <div> <Users /></div>
+    </div>
+     
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+   
+  }),
+  components:{
+    Users :()=>import("../../components/users/users.vue"),
+   
+  },
+  mounted() {
+    
+  },
+  methods: {
+  }
+};
+</script>
