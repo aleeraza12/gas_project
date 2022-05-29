e3330a5bdad9b81f24bfe2343a7ffac07b2ed2bb
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-05-29 11:12:26 +0500

    chnages

---
+++ gas_frontend/src/components/orders/no-order.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="main-container">
+        <div class="d-flex align-start justify-start">
+           <v-icon>mdi-chevron-left</v-icon> <span>Back</span>
+        </div>
+        <div class="d-flex align-center justify-center mt-3">
+             <div>
+                 <img height="300px" width="300px" src="../../assets/images/no-data.png"/>
+                 <div class="fonts"> Sorry, you have not yet created any orders <br>
+Kindly create orders to begin</div>
+<div class="mt-5 ml-16">
+                <v-btn
+                 small
+                 dense
+                  class="elevation-0 btn-create"
+                  :loading="loading"
+                
+                  @click="createAccount()"
+                >
+                  Create New Order
+                </v-btn>
+              </div>
+             </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.fonts{
+    font-size: 12px;
+}
+.btn-create {
+  background-color: #464646 !important;
+  color: #fff;
+  min-width: 300px !important;
+  border-radius: 8px !important;
+  cursor: pointer;
+}
+</style>
