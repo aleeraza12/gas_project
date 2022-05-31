d60ab91aebab12e42b7125bae0b7be7d087a04b0
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-05-31 20:43:12 +0500

    changes

--- gas_frontend/src/components/wallet/wallet.vue
+++ gas_frontend/src/components/wallet/wallet.vue
@@ -49,7 +49,7 @@
        <div class="mr-3"><b>Date Picker</b></div>
       </div>
       <div class="mt-3">
-        <v-card >
+        <v-card  class="elevation-0">
           <v-card-text>
      <v-data-table
           :headers="headers"
