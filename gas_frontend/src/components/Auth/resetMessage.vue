11bc100d6351e0870f69c6a981d926f34b1c43d8
shoaibmaqsood47 <shoaibmaqsood47@gmail.com>
2022-06-18 00:16:35 +0500

    chnasges

--- gas_frontend/src/components/Auth/resetMessage.vue
+++ gas_frontend/src/components/Auth/resetMessage.vue
@@ -1,13 +1,20 @@
 <template>
 <div class="main-container">
-  <v-card>
+  <v-card class="elevation-0">
     <v-card-text class="pa-0">
       <div class="d-flex">
-        <div class="grey-side">
-        </div>
+           <div class="grey-side">
+            <div style="position: absolute;">
+              <img src="../../assets/images/sidePic.png" height="680" width="650"/>
+            </div>
+            <div style="position:relative;margin-top:15rem" >
+              <img src="../../assets/images/side2.png" />
+            </div>
+           
+          </div>
         <div class="login-screen">
-          <div class="inner-box">
-             
+          <div  class="d-flex justify-start align-start mt-16" style="margin-left:5.5rem">
+                <img src="../../assets/images/Message Sent.png" height="200" width="200"/>
           </div>
           <div class="sign-in-content  d-flex align-start justify-start">Password Reset Successful</div>
           <div class="sign-in-subcontent  d-flex align-start justify-start">You have successfully reset your password</div>
@@ -45,12 +52,10 @@
 </script>
 <style scoped>
 .grey-side{
-  height: 100vh;
   width: 746px;
   left: 0px;
   top: 0px;
   border-radius: 0px;
-  background-color: #EBEBEA;
 
 }
 .login-screen{
@@ -86,7 +91,6 @@
  font-weight: 600;
  font-size:18px;
  color:black;
- margin-top:6rem;
  margin-left: 4.5rem;
 }
 .sign-in-subcontent {
@@ -113,11 +117,12 @@
   margin-left:23rem;
 }
 .btn-login {
-  background-color: #464646 !important;
-  color:#fff;
-  min-width:300px !important;
-  border-radius: 8px !important;
+  background-color: #2E3995 !important;
+  color: #fff;
+  min-width: 200px !important;
   cursor: pointer;
+   border-radius: 20px !important;
+  margin-left: 3rem;
 }
 .resend-text{
   margin-left:4.5rem;
