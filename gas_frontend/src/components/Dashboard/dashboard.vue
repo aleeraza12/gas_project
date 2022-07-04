99b8d6115f2654179a9c5970b8d3e88483469307
abdullah597 <abdullahjaved597@yahoo.com>
2022-07-04 12:15:04 +0500

    Changs

--- gas_frontend/src/components/Dashboard/dashboard.vue
+++ gas_frontend/src/components/Dashboard/dashboard.vue
@@ -141,6 +141,23 @@
       </div>
     </div>
 
+    <div>
+      <vue-horizontal-list :items="getRates" :options="options2">
+        <template v-slot:default="{getRate}">
+          <div>
+            <div class="Banner background-image ptb-48 p-32 border-4">
+              <div class="flex-center">
+                <div class="flex-column-align-center white text-center">
+                  <h1>{{getRate}}</h1>
+                  <p>Why the text above is important</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </vue-horizontal-list>
+    </div>
+
     <v-row>
       <v-col md="4" lg="4" xl="4" sm="12">
         <v-card height="150" style="background-color: #e3fbff">
@@ -329,6 +346,8 @@ import RequestService from "../../RequestService";
 import { mapGetters } from "vuex";
 import { eventBus } from "@/main";
 import datePicker from "../../views/Pages/datePicker.vue";
+import VueHorizontalList from "vue-horizontal-list";
+
 export default {
   data: () => ({
     nameRules: [(v) => !!v || "This field is required"],
@@ -348,6 +367,7 @@ export default {
     location: "",
     depo_name: "",
     search: "",
+    items: [0,1,2,3,4,5,6,7,8,9],
     snacbarMessage: "",
     snackbar: false,
     snackbarColor: "",
@@ -372,35 +392,50 @@ export default {
       { text: "Payment Mode", value: "payment_mode" },
       { text: "View Details", value: "actions", sortable: false },
     ],
-    options2: {
-      responsive: [
-        { end: 576, size: 1 },
-        { start: 576, end: 768, size: 2 },
-        { start: 768, end: 992, size: 3 },
-        { size: 4 },
-      ],
-      autoscroll: {
-        enabled: true,
-        interval: 5000, // in milliseconds
-        repeat: true, // used to tell if after reaching the end of the list should the list return to beginning again
-      },
-      list: {
-        // 1200 because @media (min-width: 1200px) and therefore I want to switch to windowed mode
-        windowed: 1200,
-        // Because: #app {padding: 80px 24px;}
-        padding: 24,
-      },
-      items: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => {
-        return {
-          title: `Auto scroll ${i}`,
-          content: `Auto scroll, 5s/5000ms.`,
-        };
-      }),
-    },
+    options2 : {
+  getRates: {
+  },
+  list: {
+    // css class for the parent of item
+    class: "",
+    // maximum width of the list it can extend to before switching to windowed mode, basically think of the bootstrap container max-width
+    // windowed is used to toggle between full-screen mode and container mode
+    windowed: 1200,
+    // padding of the list, if container < windowed what is the left-right padding of the list
+    // during full-screen mode the padding will added to left & right to centralise the item
+    padding: 24,
+  },
+  responsive: [
+    // responsive breakpoints to calculate how many items to show in the list at each width interval
+    // it will always fall back to these:
+    { end: 576, size: 1 },
+    { start: 576, end: 768, size: 2 },
+    { start: 768, end: 992, size: 3 },
+    { start: 992, end: 1200, size: 4 },
+    { start: 1200, size: 5 },
+  ],
+  navigation: {
+    // when to show navigation
+    // start: 992,
+    // color: "#000",
+  },
+  position: {
+    // Start from '1' on mounted.
+    start: 1,
+  },
+  autoplay: {
+    // enable/disable playing slideshow
+    play: true,
+    // the delay duration between slides in milliseconds
+    speed: 1800,
+    // if setup, the slideshow will be in the loop.
+    repeat: true,
+  },
+}
   }),
   components: {
     datePicker,
-    // VueHorizontalListAutoscroll
+    VueHorizontalList
   },
 
   mounted() {
@@ -422,6 +457,7 @@ export default {
       "getPrice",
       "getDashboardData",
       "getAllOrders",
+      "getRates"
     ]),
   },
   watch: {},
@@ -525,5 +561,10 @@ export default {
   background: #eff0fa;
   font-size: 12px;
   height: 50px;
+}
+.item {
+  padding: 24px;
+  border-radius: 3px;
+  background: #f5f5f5;
 }
 </style>
